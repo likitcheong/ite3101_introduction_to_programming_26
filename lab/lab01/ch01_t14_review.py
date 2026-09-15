38948a37bda645a85371e5faefa6c47c6c6fd168
likitcheong <likitcheong2006@gmail.com>
2026-09-15 02:31:49 +0000

    [skip actions] lab/lab01/ch01_t01_hello_world.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,15 +1,6 @@
-# 1. Create a variable called skill_completed and set it equal to the string "Python Syntax"
 skill_completed = "Python Syntax"
-
-# 2. Create exercises_completed and points_per_exercise variables
 exercises_completed = 13
 points_per_exercise = 5
-
-# 3. Create point_total variable and set it to 100
 point_total = 100
-
-# 4. Update point_total by adding the product of exercises_completed and points_per_exercise
 point_total += exercises_completed * points_per_exercise
-
-# (Optional) Print the final total to verify
-print(point_total)  # Output will be 165
+print(point_total)
