a0f43b047e87c572ad2d64149cdc2a7c6564f3df
likitcheong <likitcheong2006@gmail.com>
2026-09-15 02:07:27 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -1,5 +1,5 @@
 # Hi! I'm Maria and I live in script.py.
 # I'm an expert Python coder.
 # I'm 21 years old and I plan to program cool stuff forever.
-age_is_12 = True
-name_is_maria = False
+age_is_12 = False
+name_is_maria = Fals
